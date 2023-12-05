0a42b8060276c2cc6bef6b796bf06ab9f378bcd1
Ryan Van Massenhoven <ryanvanmassenhoven@gmail.com>
2023-12-04 20:00:52 -0500

    Add line to remove users.csv

--- assets/2023/MFA-Enforcement/lockout.ps1
+++ assets/2023/MFA-Enforcement/lockout.ps1
@@ -1,3 +1,4 @@
 Write-Output "primaryEmail,isEnrolledIn2Sv" >> users.csv
 gam print users is2svenrolled | Select-String False | Out-File users.csv -Append
 gam csv users.csv gam user ~primaryEmail signout
+Remove-Item users.csv
